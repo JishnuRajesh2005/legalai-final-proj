55279afb9545b2ff889e0935a2d4768a6277ef43
JishnuRajesh2005 <rjishnu2005@gmail.com>
2026-01-06 16:01:10 +0530

    chat section completed

---
+++ chat/context_builder.py
@@ -0,0 +1,16 @@
+from legal.parser import extract_clause
+from legal.classifier import classify_document
+
+def build_context(chat_state, current_input):
+    docs = []
+
+    for doc in chat_state.get_documents():
+        docs.append({
+            "type": classify_document(doc["content"]),
+            "clauses": extract_clause(doc["content"])[:15]
+        })
+        return {
+            "history": chat_state.get_history()[:5],
+            "documents": docs,
+            "current_input": current_input
+        }
